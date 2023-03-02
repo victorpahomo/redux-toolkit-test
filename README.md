8412206799147909c2c69e0a881623bdf69f1de8
moralesvictorr <vmmorales@unicauca.edu.co>
2023-03-01 21:15:57 -0500

    feat: initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# redux-toolkit-test
-Redux Toolkit Test
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-5xl'>Hola</h1>
+    </div>
+  )
+}
+
+export default App
